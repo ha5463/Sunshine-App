5726bf02d133cd7e1bb47ff3d433c49cd80a75b8
Ajay Agarwal <rit2015021@iiita.ac.in>
2016-10-11 01:06:24 +0530

    Implicit Intent and Share Intent added

--- Sunshine-App/app/src/main/java/com/appic/sunshine/DetailActivity.java
+++ Sunshine-App/app/src/main/java/com/appic/sunshine/DetailActivity.java
@@ -19,10 +19,13 @@ package com.appic.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,8 +43,6 @@ public class DetailActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,30 +50,16 @@ public class DetailActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Log.v("SETTING DETAIL","OK");
-            Log.v("INPUT", getString(R.string.pref_location_key));
-            startActivity(new Intent(this , SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class DetailFragment extends Fragment {
 
+        private final static String LOG_TAG = DetailFragment.class.getSimpleName();
+        private final static String FORECAST_SHARE_HASHTAG = "#SUNSHINEAPP";
+        private String mForecastStr;
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -84,11 +71,32 @@ public class DetailActivity extends ActionBarActivity {
             // The detail Activity called via intent.  Inspect the intent for forecast data.
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
             }
 
             return rootView;
         }
+        public Intent createshareForecastIntent(){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastStr+FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+            ShareActionProvider mshareactionprovider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if(mshareactionprovider != null){
+                mshareactionprovider.setShareIntent(createshareForecastIntent());
+            }
+            else{
+                Log.d(LOG_TAG, "Share Action Provider Is null");
+            }
+
+        }
+
     }
 }
--- Sunshine-App/app/src/main/java/com/appic/sunshine/MainActivity.java
+++ Sunshine-App/app/src/main/java/com/appic/sunshine/MainActivity.java
@@ -1,7 +1,10 @@
 package com.appic.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -56,10 +59,24 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
+        if(id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
-
+  private void openPreferredLocationInMap(){
+      SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+      String location = prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+      Uri geoLocation = Uri.parse("geo:0:0?").buildUpon().appendQueryParameter("q",location).build();
+      Intent intent = new Intent(Intent.ACTION_VIEW);
+      intent.setData(geoLocation);
+      if(intent.resolveActivity(getPackageManager()) != null){
+          startActivity(intent);
+      }else{
+          Log.d("ERROR INTENT MAIN", "Couldn't Call "+ location + ", no preferences");
+      }
+      }
 
 }
